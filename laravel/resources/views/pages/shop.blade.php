bf877526daca8c24c396fed9856f0d066bef63c8
Julius <julius.aguilar6@gmail.com>
2016-05-10 20:00:10 +0800

    try pero hindi pa tapos yung shop page

--- laravel/resources/views/pages/shop.blade.php
+++ laravel/resources/views/pages/shop.blade.php
@@ -1,37 +1,37 @@
 @extends('layout.master')
 @section('title', 'GTG - Shop')
-@section('content')
-<div role="main" class="page-body">
-	    <div class="title-page">
-			<span>Items</span>
-		</div>    	
-        <div class="main-wrap-shop-menu">
+@section('content')	
+<div class="main-wrap-shop-menu">
 
-			<div class="veges-img">
-				<a href="{{ url ('vegetables') }}"><label class="icon1">Fruits</label></a>
-			</div>
-			
-			<div class="fruits-img">
-				<a href="{{ url ('fruits') }}"><label class="icon1">Fruits</label></a>
-			</div>
-			
-			<div class="fish-img">
-				<a href="{{ url ('meat-fish') }}"><label class="icon1">Meat/Fish</label></a>
-			</div>
-			
-			<div class="can-img">
-				<a href="{{ url ('canned-instant-goods') }}"><label class="icon1">Canned Goods / Instant Food</label></a>
-			</div>
-			
-			<div class="rice-img">
-				<a href="{{ url ('rice') }}"><label class="icon1">Rice</label></a>
-			</div>
-			
-			<div class="others-img">
-				<a href="#"><label class="icon1">Other Services</label></a>
-			</div>            
-        </div>
-  </div>
+	<div class="title-page">
+		<span>Items</span>
+	</div> 
+
+	<div class="veges-img">
+		<a href="{{ url ('vegetables') }}"><label class="icon1">Fruits</label></a>
+	</div>
+	
+	<div class="fruits-img">
+		<a href="{{ url ('fruits') }}"><label class="icon1">Fruits</label></a>
+	</div>
+	
+	<div class="fish-img">
+		<a href="{{ url ('meat-fish') }}"><label class="icon1">Meat/Fish</label></a>
+	</div>
+	
+	<div class="can-img">
+		<a href="{{ url ('canned-instant-goods') }}"><label class="icon1">Canned Goods / Instant Food</label></a>
+	</div>
+	
+	<div class="rice-img">
+		<a href="{{ url ('rice') }}"><label class="icon1">Rice</label></a>
+	</div>
+	
+	<div class="others-img">
+		<a href="#"><label class="icon1">Other Services</label></a>
+	</div>  
+</div>
+<div style="height: 35%;"></div> 
 @endsection
 
 @section('scripts')
