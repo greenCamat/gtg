920c1b60caea0d6382229832e7060779a7104c22
Julius <julius.aguilar6@gmail.com>
2016-05-11 01:08:10 +0800

    edited style2.css and shop.blade.php

--- laravel/resources/views/pages/shop.blade.php
+++ laravel/resources/views/pages/shop.blade.php
@@ -7,7 +7,48 @@
 		<span>Items</span>
 	</div> 
 
-	<div class="veges-img">
+	<div class="upper-div">
+		<div class="left">
+			<a href="{{ url ('vegetables') }}" class="darken">
+				<img src="/images/icon/vegesB.png" style="height: 222px;width:250px;">
+				<span>Vegetables</span>
+			</a>
+		</div>
+		<div class="left">
+			<a href="{{ url ('fruits') }}" class="darken">
+				<img src="/images/icon/fruits1.png" style="height: 222px;width:250px;">
+				<span>Fruits</span>
+			</a>
+		</div>
+		<div class="left">
+			<a href="{{ url ('meat-fish') }}" class="darken">
+				<img src="/images/icon/Icon_vegies.png" style="height: 222px;width:250px;">
+				<span>Meat/Fish</span>
+			</a>
+		</div>
+	</div>
+
+	<div class="lower-div">
+		<div class="left">
+			<a href="{{ url ('canned-instant-goods') }}" class="darken">
+				<img src="/images/icon/Icon_vegies.png" style="height: 222px;width:250px;">
+				<span>Canned Goods / Instant Food</span>
+			</a>
+		</div>
+		<div class="left">
+			<a href="{{ url ('rice') }}" class="darken">
+				<img src="/images/icon/Icon_vegies.png" style="height: 222px;width:250px;">
+				<span>Rice</span>
+			</a>
+		</div>
+		<div class="left">
+			<a href="{{ url ('meat-fish') }}" class="darken">
+				<img src="/images/icon/Icon_vegies.png" style="height: 222px;width:250px;">
+				<span>Other Services</span>
+			</a>
+		</div>
+	</div>
+	<!--<div class="veges-img">
 		<a href="{{ url ('vegetables') }}"><label class="icon1">Fruits</label></a>
 	</div>
 	
@@ -29,7 +70,7 @@
 	
 	<div class="others-img">
 		<a href="#"><label class="icon1">Other Services</label></a>
-	</div>  
+	</div>  -->
 </div>
 <div style="height: 35%;"></div> 
 @endsection
