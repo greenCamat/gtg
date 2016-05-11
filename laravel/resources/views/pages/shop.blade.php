4fde22e97eb52e9aa4e4c33861d95f1c0c71ca29
Julius <julius.aguilar6@gmail.com>
2016-05-11 12:37:38 +0800

    changes

--- laravel/resources/views/pages/shop.blade.php
+++ laravel/resources/views/pages/shop.blade.php
@@ -10,24 +10,24 @@
 	<div class="upper-div">
 		<div class="left">
 			<a href="{{ url ('vegetables') }}" class="darken">
-				<img src="/images/icon/vegesB.png" style="height: 222px;width:250px;">
+				<img src="/images/icon/vegiesiconred.png" style="height: 222px;width:250px;">
 				<span>Vegetables</span>
 			</a>
 		</div>
 		<div class="left">
 			<a href="{{ url ('fruits') }}" class="darken">
-				<img src="/images/icon/fruits1.png" style="height: 222px;width:250px;">
+				<img src="/images/icon/fruitsicon.png" style="height: 222px;width:250px;">
 				<span>Fruits</span>
 			</a>
 		</div>
 		<div class="left">
 			<a href="{{ url ('meat-fish') }}" class="darken">
 				<img src="/images/icon/Icon_vegies.png" style="height: 222px;width:250px;">
-				<span>Meat/Fish</span>
+				<span>Meat/Fish/Chicken and Frozen Foods</span>
 			</a>
 		</div>
 	</div>
-
+<div style="height: 55%;"></div> 
 	<div class="lower-div">
 		<div class="left">
 			<a href="{{ url ('canned-instant-goods') }}" class="darken">
@@ -48,31 +48,8 @@
 			</a>
 		</div>
 	</div>
-	<!--<div class="veges-img">
-		<a href="{{ url ('vegetables') }}"><label class="icon1">Fruits</label></a>
-	</div>
-	
-	<div class="fruits-img">
-		<a href="{{ url ('fruits') }}"><label class="icon1">Fruits</label></a>
-	</div>
-	
-	<div class="fish-img">
-		<a href="{{ url ('meat-fish') }}"><label class="icon1">Meat/Fish</label></a>
-	</div>
-	
-	<div class="can-img">
-		<a href="{{ url ('canned-instant-goods') }}"><label class="icon1">Canned Goods / Instant Food</label></a>
-	</div>
-	
-	<div class="rice-img">
-		<a href="{{ url ('rice') }}"><label class="icon1">Rice</label></a>
-	</div>
-	
-	<div class="others-img">
-		<a href="#"><label class="icon1">Other Services</label></a>
-	</div>  -->
 </div>
-<div style="height: 35%;"></div> 
+<div style="height: 80%;"></div> 
 @endsection
 
 @section('scripts')
