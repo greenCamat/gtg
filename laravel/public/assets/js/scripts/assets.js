5c98e2b0408e389fb69e7a9486b1defcf1d853e0
Jaymar <camat21@gmail.com>
2016-05-13 16:35:06 +0800

    Fix the error for the slider jquery

--- laravel/public/assets/js/scripts/assets.js
+++ laravel/public/assets/js/scripts/assets.js
@@ -1,6 +1,6 @@
 $.noConflict();
-(function($){
-    $('.slider2').bxSlider({
+jQuery(document).ready(function(){
+  jQuery('.slider2').bxSlider({
 
     slideWidth: 780,
     minSlides: 1,
@@ -12,19 +12,5 @@ $.noConflict();
     autoControls: true
 
   });
-})(jQuery);
-// jQuery(document).ready(function(){
-  // jQuery('.slider2').bxSlider({
-
-    // slideWidth: 780,
-    // minSlides: 1,
-    // maxSlides: 1,
-    // default: 'horizontal',
-    // mode: 'fade',
-    // slideMargin: 10,
-    // auto: true,
-    // autoControls: true
-
-  // });
-// });
+});
 
