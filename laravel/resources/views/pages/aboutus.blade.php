6f2a8c8791d7fb99129794df8f7fdcfa6b22680a
Julius <julius.aguilar6@gmail.com>
2016-06-08 18:15:34 +0800

    some changes

--- laravel/resources/views/pages/aboutus.blade.php
+++ laravel/resources/views/pages/aboutus.blade.php
@@ -1,13 +1,22 @@
 @extends('layout.master')
 @section('title', 'GTG - About Us')
 @section('content')
-<div class="main-wrap-shop-menu">
-    <div class="contactus">
-        <div class="contactus-content">
-            <div class="gtg-mission">Mission</div>
-
-        </div>
-    </div>
+<div role="main" class="page-body">
+	<div class="main-wrap-shop-menu">
+	    <div class="contactus">
+	        <div class="contactus-content">
+	            <div class="gtg-mission">
+	            	<label>Our Mission</label>
+		            <p>To Satisfy the needs of the people and brings happiness to your home.</p>
+		            <label>Our Vision</label>
+		            <p>- To attain the highest satisfaction of our customers.</p>
+		            <p>- To deliver quality goods to our customers.</p>
+		            <p>- To build good relationship towards customers.</p>
+		            <label>WHAT WE DO?</label>
+		            <p>We deliver fresh groceries at your door step.</p>
+	            </div>
+	        </div>
+	    </div>
+	</div>
 </div>
-
 @endsection
